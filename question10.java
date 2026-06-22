a68734342991d23c48f98dfd895302f7bddd29e9
Jidthesh V <jidtheshv10-hash  he/him@gmail.com>
2026-06-22 10:17:49 +0530

    adding java files

---
+++ question10.java
@@ -0,0 +1,13 @@
+public class question10 {
+    public static void main(String[] args) {
+
+        int n = 8;
+
+        if((n & (n - 1)) == 0) {
+            System.out.println(n + " is a Power of 2");
+        } else {
+            System.out.println(n + " is NOT a Power of 2");
+        }
+    }
+}
+
