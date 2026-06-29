177d2f7b6e9a182c5b159bb5f50a297d7dc47bd1
Jidthesh V <jidtheshv10-hash  he/him@gmail.com>
2026-06-29 12:21:06 +0530

    adding java filess

---
+++ payment.java
@@ -0,0 +1,35 @@
+interface paymt {
+    void pay();
+    
+}
+class upi implements paymt{
+    public void upi()
+    {
+        System.out.println("Method is upi");
+    }
+}
+class card implements paymt
+{
+    public void card()
+    {
+        System.out.println("Method is card ");
+    }
+}
+class cash implements paymt{
+    public void cash()
+    {
+        System.out.println("Method is cash ");
+    }
+}
+
+public class payment
+{
+    public static void main(String[] args) {
+        cash c= new cash();
+        c.cash();
+        upi u= new upi();
+        u.upi();
+        card d= new card();
+        d.card();
+    }
+}
