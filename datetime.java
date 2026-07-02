7a97426c979c958b0879b188dd1ddb60c99fe91c
Jidthesh V <jidtheshv10-hash  he/him@gmail.com>
2026-07-02 14:15:12 +0530

    adding java files

---
+++ datetime.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+public class datetime
+{
+    public static void main(String [ ]args)
+    {
+        LocalDateTime d= LocalDateTime.now();
+        System.out.println(d);
+    }
+}
+
